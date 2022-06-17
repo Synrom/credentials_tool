a17f2b056ea377fff18dabd45fb820a42e8ba56a
max.leiwig <s6maleiw@uni-bonn.de>
2022-06-17 21:10:14 +0200

    minor fixes in item parsing

--- credentials_tool/credential_format.py
+++ credentials_tool/credential_format.py
@@ -52,11 +52,14 @@ class LineInterpreterPasswordMail(AbstractLineInterpreterCredential):
         item_splited = item.split(":")
 
         if len(item_splited) < 2:
+            print(item+" not aproved by PasswordMail")
             return False
 
         if self.email_regex.match(item_splited[-1]):
+            print(item+" aproved by PasswordMail")
             return True
 
+        print(item+" not aproved by PasswordMail")
         return False
 
     def interpret_item(self, item: str) -> CredentialHolder:
@@ -64,9 +67,11 @@ class LineInterpreterPasswordMail(AbstractLineInterpreterCredential):
         item_splited = item.split(":")
 
         if len(item_splited) < 2:
+            print(item+" error in PasswordMail < 2")
             raise InterpreterFormatError(item+" is not in the right format <password>:<email>")
 
-        password = "".join(item_splited[:-1])
+        print(item+" no error in PasswordMail")
+        password = ":".join(item_splited[:-1])
         mail = item_splited[-1]
 
         return CredentialHolder(email=mail, password=password)
@@ -82,11 +87,14 @@ class LineInterpreterMailPassword(AbstractLineInterpreterCredential):
         item_splited = item.split(":")
 
         if len(item_splited) < 2:
+            print(item+" item not aproved by MailPassword")
             return False
 
         if self.email_regex.match(item_splited[0]):
+            print(item+" item aproved by MailPassword")
             return True
 
+        print(item+" item not aproved MailPassword")
         return False
 
     def interpret_item(self, item: str) -> CredentialHolder:
@@ -94,10 +102,12 @@ class LineInterpreterMailPassword(AbstractLineInterpreterCredential):
         item_splited = item.split(":")
 
         if len(item_splited) < 2:
+            print(item+" error in MailPassword < 2")
             raise InterpreterFormatError(item+" is not in the right format <email>:<password>")
 
+        print(item+" no error in MailPassword")
         mail = item_splited[0]
-        password = "".join(item_splited[1:])
+        password = ":".join(item_splited[1:])
 
         return CredentialHolder(email=mail, password=password)
 
